a6ea6317e461238bfe59fbe2a255d641db4f8104
Revegouk Roman <romarevegouk@gmail.com>
2019-12-12 19:15:22 +0200

    Hw14 is done! Multithreading

---
+++ src/Lesson14/hw13_Task4/HW13_Task4_SingleMax.java
@@ -0,0 +1,17 @@
+package Lesson14.hw13_Task4;
+
+public class HW13_Task4_SingleMax {
+
+	public int foriMaxDetermination(int[][] matrix) {
+		int max = 0;
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (matrix[i][j] > max)
+					max = matrix[i][j];
+			}
+		}
+		return max;
+	}
+
+}
